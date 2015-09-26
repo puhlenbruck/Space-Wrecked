4be35aafcc13174cbeb677c05aacd47bda6c0773
Peter Uhlenbruck <peuhlenb@gmail.com>
2015-09-26 00:47:50 -0700

    Create Shuttle Room

--- world.js
+++ world.js
@@ -1,6 +1,9 @@
 function world(){
 	this.map = {};
 	this.map["0,0"] = new room(0,0);
+	this.map["0,0"].description = "Crash Site"
+	this.map["shuttle"] = new room(0,0); // shuttle interior
+	this.map["shuttle"].description = "Inside The Shuttle";
 	
 	this.getRoom = function(x,y){
 		key = createCoordString(x,y);
@@ -16,7 +19,7 @@ function world(){
 
 function room(x,y){
 	this.loc = [x,y];
-	this.description = "";
+	this.description = "Room at (" + x + "," + y + ")";
 	this.lastVisited = 0;
 	this.lastChanged = 0;
 	this.changes = [];
